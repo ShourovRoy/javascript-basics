8150eab6383e91caa04d12776e0df468c5efc0b2
Shourov Roy <shourovroy2389@gmail.com>
2021-09-09 01:01:15 +0600

    class One

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// hello I am a comment
+
+// var store = document.querySelector("#heading");
+
+// function hitme() {
+//   store.innerText = "Hello I am from js";
+// }
+
+// learn variable
+// var a = 12
+// var b = 2
+
+// var total = a + b
+
+// console.log(total, a, b)
+// var heroname = "Clark"
+// var unclename = "Ben"
+
+// var golpo = `The name of Sipderman is ${heroname}, and Petter's uncle is ${unclename}`
+
+// console.log(golpo)
+// var sentence = `
+// Hey guys,
+// I am Shourov. How are you??
+
+// And how is going everything?
+// `
+
+// console.log(sentence)
+
+
+
+
+
